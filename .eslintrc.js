7580b790cd527f83085d3ab915d11d5302415e9d
“Chi <bennylin77@gmail.com>
2019-10-24 07:16:18 +0000

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,41 @@
+const path = require('path');
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: [
-    'airbnb',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
-};
+    "extends": "airbnb",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-hooks"
+    ],
+    "rules": {
+        "indent": ["error", "tab"],
+        "max-len": [2, {"code": 160}],
+        "no-tabs": ["error", { "allowIndentationTabs": true }],
+        "import/extensions": ["error", "never", {"jsx": "always"}],
+        "jsx-quotes": ["error", "prefer-single"],
+        "operator-linebreak": ["error", "after"],
+        "object-curly-newline": ["error", { "multiline": true, "consistent": true }],
+        "react/jsx-indent-props": "off",
+        "react/jsx-indent": "off",
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "react/jsx-curly-spacing": [2, {"when": "always"}],
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",        
+        "import/prefer-default-export": "off",
+    },
+    "parser": "babel-eslint"
+}
